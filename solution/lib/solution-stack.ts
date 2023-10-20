987843b76fc2dbd44e59ca3d0fcc80b4886fee65
Eduardo Patrocinio <epatro@amazon.com>
2023-10-20 09:54:18 -0400

    Create queue

--- solution/lib/solution-stack.ts
+++ solution/lib/solution-stack.ts
@@ -2,10 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as Sns from 'aws-cdk-lib/aws-sns';
+import * as Sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SolutionStack extends cdk.Stack {
   private vpc: Vpc;
   private topic: Sns.Topic;
+  private sqs: Sqs.Queue;
 
   private createVpc() {
     this.vpc = new Vpc (this, 'card-auth');
@@ -15,9 +17,14 @@ export class SolutionStack extends cdk.Stack {
     this.topic = new Sns.Topic(this, 'Authorization')
   }
 
+  private createSqs() {
+    this.sqs = new Sqs.Queue (this, 'Same Region Queue');
+  }
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     this.createVpc();
     this.createSns();
+    this.createSqs();
   }
 }
