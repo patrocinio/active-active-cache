a84e8db78998936a2e29e0deb5d4ba97874dbdb9
Eduardo Patrocinio <epatro@amazon.com>
2023-10-20 21:36:13 -0400

    Create ElastiCache

--- solution/lib/solution-stack.ts
+++ solution/lib/solution-stack.ts
@@ -3,11 +3,13 @@ import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import * as Sns from 'aws-cdk-lib/aws-sns';
 import * as Sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_elasticache as ElastiCache } from 'aws-cdk-lib'
 
 export class SolutionStack extends cdk.Stack {
   private vpc: Vpc;
   private topic: Sns.Topic;
   private sqs: Sqs.Queue;
+  private cache: ElastiCache.CfnCacheCluster;
 
   private createVpc() {
     this.vpc = new Vpc (this, 'card-auth');
@@ -21,10 +23,28 @@ export class SolutionStack extends cdk.Stack {
     this.sqs = new Sqs.Queue (this, 'Same Region Queue');
   }
 
+  private createElastiCache() {
+    const groupName = "ElastiCacheGroupName";
+
+    const subnetGroup = new ElastiCache.CfnSubnetGroup(this, "ElastiCacheSubnetGroup", {
+      cacheSubnetGroupName: groupName,
+      subnetIds: ["subnet-09dc13d737832aa95"],
+      description: "ElastiCache Subnet Group"
+    })
+
+    this.cache = new ElastiCache.CfnCacheCluster (this, "ElastiCache", {
+      cacheNodeType: 'cache.t2.small',
+      engine: 'redis',
+      numCacheNodes: 1,
+      cacheSecurityGroupNames: ['default'],
+    });
+  }
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     this.createVpc();
     this.createSns();
     this.createSqs();
+    this.createElastiCache();
   }
 }
