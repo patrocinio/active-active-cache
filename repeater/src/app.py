5d8142bfa5576b9cc8f0e2d3c49aca1f6ab904c3
Eduardo Patrocinio <epatro@amazon.com>
2023-11-27 20:13:32 -0300

    Decouple repeater from loader, by calling directly SNS

--- repeater/src/app.py
+++ repeater/src/app.py
@@ -5,14 +5,6 @@ import json
 
 def lambda_handler(event, context):
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
     req = urllib.request.Request(
         url='https://fs4mfkm0pa.execute-api.us-west-2.amazonaws.com/Prod/send/',
         headers={'Accept': 'application/json'},
@@ -26,10 +18,3 @@ def lambda_handler(event, context):
         print("Response: ", response)
         time.sleep(1)
     
-
-#    return {
-#        "statusCode": 200,
-#        "body": json.dumps({
-#            "message": response
-#        }),
-#    }
